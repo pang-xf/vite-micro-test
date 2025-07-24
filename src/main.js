9577d40c38b96428399aa73215f6396778ec1bf6
庞晓峰 <335136854@qq.com>
2025-07-24 09:57:48 +0800

    fox

--- src/main.js
+++ src/main.js
@@ -3,3 +3,11 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+export default function mount(container) {
+  app.mount(container)
+}
+
+export const unmount = () => {
+  app.unmount()
+}
